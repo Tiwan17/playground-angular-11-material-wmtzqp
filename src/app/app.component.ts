2b854789aa11438e591295f586b3b921efdf50a6
Tiwan17 <93998959+Tiwan17@users.noreply.github.com>
2023-03-30 11:49:21 +0200

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon | undefined;
-
-  ngOnInit() {
-    console.table(this.pokemonList);
-  }
-
-  selectPokemon(pokemonId: string) {
-    const pokemon: Pokemon | undefined = this.pokemonList.find(
-      (pokemon) => pokemon.id == +pokemonId
-    );
-    if (pokemon) {
-      console.log(`test : ${pokemon.name}`);
-      this.pokemonSelected = pokemon;
-    } else {
-      console.log(`Nope`);
-      this.pokemonSelected = pokemon;
-    }
-  }
-}
+export class AppComponent {}
--- src/app/list-pokemon/list-pokemon.component.ts
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-list-pokemon',
   templateUrl: './list-pokemon.component.html',
-  styles: [],
 })
-export class ListPokemonComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class ListPokemonComponent{
+  pokemonList: Pokemon[] = POKEMONS;
 }
