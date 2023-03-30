f958107e2c63b5f5be78d26faffb97310c44ba9e
Tiwan17 <93998959+Tiwan17@users.noreply.github.com>
2023-03-30 10:07:02 +0200

    1 file modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageHeadingComponent } from './page-heading/page-heading.component';
 import { TimeDurationPipe } from './timeDuration.pipe';
@@ -9,6 +10,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { BorderCardDirective } from './border-card.directive';
 import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
+import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, MatButtonModule, MatTooltipModule],
@@ -18,6 +21,8 @@ import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
     TimeDurationPipe,
     BorderCardDirective,
     PokemonTypeColorPipe,
+    ListPokemonComponent,
+    DetailPokemonComponent,
   ],
   bootstrap: [AppComponent],
 })
